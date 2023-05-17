86f60e9dc807b1fe681e3fafe3ff854c99fe585b
mehmetfilik <filikmehmet02gmail.com>
2023-05-17 20:39:25 +0200

    230517

---
+++ src/test/java/tests/day14_TestNGFrameworkOlusturma/C01_Priority.java
@@ -0,0 +1,35 @@
+package tests.day14_TestNGFrameworkOlusturma;
+
+import org.testng.annotations.Test;
+
+public class C01_Priority {
+    /*
+        JUnit framework'de testlerin calisma sirasi
+        ONGORULEMEZ ve DUZENLENEMEZ
+
+        TestNG testleri harf sirasina gore calistirir
+
+        Ayrica priority degeri girerek testleri istedigimiz sira ile calistirabiliriz
+        (KONTROL EDILEBILIR SIRALAMA)
+
+        priority sayisal olarak kucukten buyuge dogru testleri calistirir
+     */
+
+    @Test(priority = 57)
+    public void youtubeTesti(){
+        System.out.println("Youtube");
+    }
+
+    @Test(priority = -3)
+    public void wiseTesti(){
+        System.out.println("Wise Quarter");
+    }
+
+    @Test(priority = 57)
+    public void amazonTesti(){ // priority yazilmazsa 0 degeri alir
+        System.out.println("Amazon");
+    }
+
+
+
+}
