4059bf44e2faa6ec50049ceab84c0ddea306fcaa
mehmetfilik <filikmehmet02gmail.com>
2023-05-18 20:41:38 +0200

    230518

--- src/test/java/tests/day15_POM/C03_QualitydemyNegatifLoginTesti.java
+++ src/test/java/tests/day15_POM/C03_QualitydemyNegatifLoginTesti.java
@@ -1,7 +1,39 @@
 package tests.day15_POM;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+
 public class C03_QualitydemyNegatifLoginTesti {
 
+    @Test
+    public void test01(){
 
+        // qualitydemy anasayfa'ya gidin
+        Driver.getDriver().get("https://www.qualitydemy.com");
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        // ilk login linine tiklayin
+        qualitydemyPage.ilkLoginLinki.click();
+
+        // kullanici adi olarak selenium
+        qualitydemyPage.emailKutusu.sendKeys("selenium");
+
+        // password olarak heyecandir yazin
+        qualitydemyPage.passwordKutusu.sendKeys("heyecandir");
+
+        // login butonuna tiklayin
+        qualitydemyPage.loginButonu.click();
+
+        // giris yapilamadigini test edin
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isEnabled());
+
+        // sayfayi kapatin
+
+        Driver.closeDriver();
+
+
+
+    }
 
 }
