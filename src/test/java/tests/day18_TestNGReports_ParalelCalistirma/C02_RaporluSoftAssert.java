9ae2385cfa9e09a014f8a757d2312f4a7c148e80
mehmetfilik <filikmehmet02gmail.com>
2023-05-23 20:19:50 +0200

    230523

--- src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C02_RaporluSoftAssert.java
+++ src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C02_RaporluSoftAssert.java
@@ -1,8 +1,10 @@
 package tests.day18_TestNGReports_ParalelCalistirma;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.Driver;
 import utilities.TestBaseRapor;
 
@@ -11,18 +13,49 @@ public class C02_RaporluSoftAssert extends TestBaseRapor {
     @Test
     public void test01() throws InterruptedException {
         // 1. Test
+        extentTest=extentReports.createTest("Alert test",
+                                            "Kullanici JS alert'leri calistirabilmeli ve kapatabilmeli");
+
         //            -  https://the-internet.herokuapp.com/javascript_alerts adresine gidin
         Driver.getDriver().get("https://the-internet.herokuapp.com/javascript_alerts");
+        extentTest.info("Kullanici herokuapp alert sayfasina gider");
+
         //            - 1.alert'e tiklayin
         Thread.sleep(3000);
-        Driver.getDriver().findElement(By.xpath("//*[text()='Click for JS Alert']")).click();
+        WebElement ilkAlertElementi = Driver.getDriver().findElement(By.xpath("//*[text()='Click for JS Alert']"));
+        ilkAlertElementi.click();
+        extentTest.info("Ilk alert butonuna click yapar");
+
         //            -  Alert'deki yazinin "I am a JS Alert" oldugunu test edin
         Thread.sleep(3000);
+
         String expectedAlertYazisi = "I am a JS Alert";
         String actualAlertYazisi = Driver.getDriver().switchTo().alert().getText();
-        Assert.assertEquals(expectedAlertYazisi,actualAlertYazisi);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        // Assert.assertEquals(expectedAlertYazisi,actualAlertYazisi);
+        softAssert.assertEquals(actualAlertYazisi,expectedAlertYazisi,"Alert'teki yazi belirlenenden farkli");
+        extentTest.info("soft assert ile alert yazisinin beklenen degere sahip oldugunu test eder");
+
         //            -  OK tusuna basip alert'i kapatin
         Driver.getDriver().switchTo().alert().accept();
+        extentTest.info("alert'i kapatir");
+
+        // allert'in kapatildigii test edin
+
+        // Assert.assertTrue((ilkAlertElementi.isEnabled()));
+
+        softAssert.assertTrue(ilkAlertElementi.isEnabled(),"Alert kapatilamadi");
+        extentTest.info("soft assert ile alert'in kapatildigini test eder");
+        softAssert.assertAll();
+        extentTest.pass("soft assert ile yapilan tum testlerin sonucunu raporlar");
+
+        Driver.closeDriver();
+        extentTest.info("sayfayi kapatir");
+
+
+
     }
 
 
